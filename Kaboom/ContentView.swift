7864d774a17975f138fb4932c4f3c7694b01d972
d1onnisss <lim_al@auca.kg>
2023-11-20 16:12:39 +0600

    Initial Commit

---
+++ Kaboom/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Kaboom
+//
+//  Created by Alexey Lim on 20/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Kaboom/KaboomApp.swift
@@ -0,0 +1,17 @@
+//
+//  KaboomApp.swift
+//  Kaboom
+//
+//  Created by Alexey Lim on 20/11/23.
+//
+
+import SwiftUI
+
+@main
+struct KaboomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
